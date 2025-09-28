622264b4a63e4a6a2e06ca1d6afdc98458b39678
mandyreed223 <mandy.reed223@gmail.com>
2025-09-28 14:04:23 -0400

    Update start.sh
    Updated script after locating some needed updates

--- start.sh
+++ start.sh
@@ -16,15 +16,14 @@ wget https://download.dokuwiki.org/src/dokuwiki/dokuwiki-stable.tgz
 # Rename
 mv dokuwiki-stable.tgz dokuwiki.tgz
 
-# Exact to webroot
+# Extract to webroot
 sudo tar xpvf dokuwiki.tgz --strip-components=1 -C /var/www/html
 
 # Create wiki pages
-echo "DokuWiki project for LUIT" > /var/www/html/data/pages/start.txt
-echo "Welcome to Mandy's wiki page" > /var/www/html/data/pages/home.txt
-echo -e "IAM: Manage users/roles\n- EC2: Elastic Compute Cloud in AWS\n- S3: Bucket storage in AWS" > /var/www/html/data/pages/aws_notes.txt
-echo -e "ls - list files and folders\n- cd - change directory\n- pwd - what folder you are in\n- mkdir - make directory" > /var/www/html/data/pages/linux_commands.txt
-
+echo "DokuWiki project for LUIT" | sudo tee /var/www/html/data/pages/start.txt
+echo "Welcome to Mandy's wiki page" | sudo tee /var/www/html/data/pages/home.txt
+echo -e "  * IAM: Manage users/roles\n  * EC2: Elastic Compute Cloud in AWS\n  * S3: Bucket storage in AWS" | sudo tee /var/www/html/data/pages/aws_notes.txt
+echo -e "  * ls - list files and folders\n  * cd - change directory\n  * pwd - what folder you are in\n  * mkdir - make directory" | sudo tee /var/www/html/data/pages/linux_commands.txt
 
 # Set permissions
 sudo chown -R apache:apache /var/www/html
